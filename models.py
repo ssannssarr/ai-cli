0340e2055c3725263f7809975556878c91cddada
Santanu Sarkar <ssantanusarkar8@gmail.com>
2026-05-04 00:16:48 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 MODELS = {
     "coding": "openai/gpt-oss-120b:free",
-    "reasoning": "openai/gpt-oss-20b:free",
+    "reasoning": "openai/gpt-oss-120b:free",
     "fallback": "openai/gpt-oss-120b:free",
     "fallback2": "openai/gpt-oss-120b:free",
     "fallback3": "openai/gpt-oss-120b:free"
